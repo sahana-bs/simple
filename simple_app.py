cf1137fe83f56be27b03725a4eba1180789c5948
Sahana BS <zwicky@Mac.lan>
2025-07-04 11:50:43 -0400

    Add files for simple app

---
+++ simple_app.py
@@ -0,0 +1,23 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash-001",
+    temperature=2,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+
+messages = [
+    (
+        "system",
+        "You are a helpful assistant that translates English to French. Translate the user sentence.",
+    ),
+    ("human", "This is my first langchain code, hurry"),
+]
+ai_msg = llm.invoke(messages)
+ai_msg
+
+print(ai_msg.content)
+
